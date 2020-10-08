cdd102121a8347b5fe5c139d027a232fd5af999a
E-terry <e.terry1135@gmail.com>
2020-10-08 18:54:46 -0400

    Pushing to get code to my desktop. Still WIP stuff. I will give an actual changelog with my next push.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Development/ET/SlimChassisV3/SlimChassisDrivetrainInitializer.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Development.ET.SlimChassisV3;
+
+import org.firstinspires.ftc.teamcode.Development.ET.SlimChassisV3.ETControl.MotorInitializer;
+
+public class SlimChassisDrivetrainInitializer extends MotorInitializer {
+    @Override
+    public void init() {
+
+    }
+}
